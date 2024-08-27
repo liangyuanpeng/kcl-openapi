9e60b102beb74b526d1731399cd1de4ad0abe789
Lan Liang <gcslyp@gmail.com>
2024-08-27 03:07:25 +0000

    u

--- pkg/swagger/generator/model_test.go
+++ pkg/swagger/generator/model_test.go
@@ -1,7 +1,22 @@
 package generator
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestMakeGenDefinition(t *testing.T) {
-	// makeGenDefinition()
+
+	opts := &GenOpts{}
+	opts.Spec = "testdata/unit/model/swagger.json"
+	doc, err := opts.loadSpec()
+	if err != nil {
+		panic(err)
+	}
+	model, err := makeGenDefinition("io.k8s.api.core.v1.Namespace", "models", *doc.Schema(), doc, opts)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Println("model:", model)
 }
